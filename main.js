4c3d1c6345b6130f532af994cb96b49bd65b2c00
Cottonwood <blog_geon@naver.com>
2021-10-26 23:06:44 +0900

    정규표현식

---
+++ main.js
@@ -0,0 +1,12 @@
+let str = `
+010-2831-6734
+geon0529@naver.com
+https://cottonwood-moa.tistory.com/
+The quick brown fox jumps over the lazy dog.
+bbcccddddthe
+`
+
+console.log(
+  str.match(/fox|dog/g)
+  )
+  
